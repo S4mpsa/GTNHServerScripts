d433091dfc4ac2037b9262398a307f449100b222
OrderedSet <orderedset86@gmail.com>
2023-07-02 10:42:29 -0700

    Update backup scripts for new server
    1. Backup scripts go in parent folder of server
    2. Get rid of a bunch of user input by moving it to tz_config.yaml now
    3. Require new package (pip install PyYAML)

--- RestoreScripts/_utils.py
+++ RestoreScripts/_utils.py
@@ -4,28 +4,54 @@ from pathlib import Path
 from zipfile import ZipFile
 
 import pendulum
+from yaml import safe_load
+
+
+def loadConfig():
+    own_path = Path(__file__)
+    config_path = own_path.parent / 'config.yaml'
+    with open(config_path, 'r') as f:
+        config = safe_load(f)
+
+    # Post-processing
+    config['tz'] = config['tz'].strip()
+
+    # Sanity checks
+    assert config['use'] in ['utc', 'tz'], '"use" must be either "utc" or "tz" in config.yaml.'
+    if config['use'] == 'tz':
+        assert config['tz'] in pendulum.timezones, '\n'.join([
+            f'Invalid timezone: {config["tz"]}.',
+            f'Valid timezones:',
+            f'{pendulum.timezones}',
+        ])
+
+    server_path = own_path.parent / config['server_folder']
+    backups_path = server_path / 'backups'
+    if not server_path.exists():
+        raise RuntimeError(f'No server folder at path: {server_path.absolute()}')
+    if not backups_path.exists():
+        raise RuntimeError(f'No backups at path {backups_path.absolute()}')
+    
+    return config
 
 
 def getBackupPath():
-    server_name = input('Please enter server name (accepted: GDCloud, server, skyblock,ballers). ')
-    assert server_name in ['GDCloud', 'server', 'skyblock','ballers']
-    server_files_lookup = {
-        'GDCloud': 'World',
-        'server': 'Horizons',
-        'skyblock': 'Skyblock',
-        'ballers': 'Gateworld',
-    }
-    horizons_name = server_files_lookup[server_name]
+    config = loadConfig()
 
-    server_root = Path(__file__).parent / server_name
+    server_root = Path(__file__).parent / config['server_folder']
     backup_path = server_root / 'backups'
+    horizons_name = config['horizons_name']
 
     timezone = input('Enter Pendulum timezone to convert backup dates to (eg US/Pacific): ')
+    if config['use'] == 'utc':
+        timezone = config['utc-offset']
+    elif config['use'] == 'tz':
+        timezone = config['tz']
 
     last_5 = sorted(os.listdir(backup_path))
     print('Select backup:')
     for idx, time_str in enumerate(last_5):
-        print('   ', idx+1, time_str, f'({utfToTZ(time_str, timezone)})')
+        print('   ', idx+1, time_str, f'({serverTimestampToTZ(time_str, timezone)})')
 
     usr_in = input('Index: ')
     usr_idx = int(usr_in) - 1
@@ -41,14 +67,20 @@ def getBackupPath():
     )
 
 
-def utfToTZ(utc_str, tz):
+def serverTimestampToTZ(utc_str, tz):
+    # I don't understand why this is UTC+1 when both Helsinki and Falkenstein
+    #   are UTC+2, but it works...
     utc_str += '-+0100'
     backup_date = pendulum.from_format(utc_str, 'YYYY-MM-DD-HH-mm-ss-ZZ')
+
     converted = backup_date.in_tz(tz=tz).format('YYYY-MM-DD HH:mm')
+
     return converted
 
 
 def standardUnzip(server_root, backup_zip_path, unzipped_backup_path, horizons_name):
+    config = loadConfig()
+
     if unzipped_backup_path.exists():
         print('Backup already extracted! Using cached version.')
     else:
@@ -58,9 +90,12 @@ def standardUnzip(server_root, backup_zip_path, unzipped_backup_path, horizons_n
 
     horizons_backup_path = server_root / f'{horizons_name}.bak'
     if horizons_backup_path.exists():
-        yn = input(f'Delete old {horizons_name}.bak folder from previous backup restore? [y/n] ')
-        if yn in ['y', 'Y']:
-            shutil.rmtree(horizons_backup_path) 
+        if config['autodelete_old_bak_folder']:
+            shutil.rmtree(horizons_backup_path)
         else:
-            raise RuntimeError('Please fix accordingly and start over.')
+            yn = input(f'Delete old {horizons_name}.bak folder from previous backup restore? [y/n] ')
+            if yn in ['y', 'Y']:
+                shutil.rmtree(horizons_backup_path) 
+            else:
+                raise RuntimeError('Please fix accordingly and start over.')
 
--- RestoreScripts/restore_playerdata.py
+++ RestoreScripts/restore_playerdata.py
@@ -11,7 +11,7 @@ standardUnzip(*backup_paths)
 # Want to just overwrite the playerdata file for a specific player
 UUID = input('Please enter full UUID of player.\nYou can look up full UUID here: mcuuid.net\n')
 old_UUID_path = unzipped_backup_path / f'playerdata/{UUID}.dat'
-corrupted_UUID_path = server_root / f'Horizons/playerdata/{UUID}.dat'
+corrupted_UUID_path = server_root / f'{horizons_name}/playerdata/{UUID}.dat'
 
 if old_UUID_path.exists():
     shutil.move(old_UUID_path, corrupted_UUID_path)
