2ed9061bc35f889be0fb02ac3db5656e93121bb2
OrderedSet <orderedset86@gmail.com>
2023-08-04 21:03:26 -0700

    Add restore push utility

---
+++ copy_restore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp RestoreScripts/* /home/minecraft/gtnh/servers/ballers
+cp RestoreScripts/* /home/minecraft/gtnh/servers/gdcloud
+cp RestoreScripts/* /home/minecraft/gtnh/servers/greesy
+cp RestoreScripts/* /home/minecraft/gtnh/servers/runakai
+cp RestoreScripts/* /home/minecraft/gtnh/servers/skyblock
+cp RestoreScripts/* /home/minecraft/gtnh/servers/theepicgamer
+chown -R sampsa /home/minecraft/gtnh/servers/ballers
+chown -R gdcloud /home/minecraft/gtnh/servers/gdcloud
+chown -R greesy /home/minecraft/gtnh/servers/greesy
+chown -R runakai /home/minecraft/gtnh/servers/runakai
+chown -R orderedset /home/minecraft/gtnh/servers/skyblock
+chown -R teg /home/minecraft/gtnh/servers/theepicgamer
